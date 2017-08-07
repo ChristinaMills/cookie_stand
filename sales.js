369572884097a5d81cd8c5debd2247aae1195903
CHRISTINA MILLS <CHRISTINA.A.MILLS@GMAIL.COM>
2017-08-07 16:37:57 -0700

    got DOM to display list items and hours

--- sales.js
+++ sales.js
@@ -18,11 +18,12 @@ var pdxAirport = {
     },
 
     addToDom: function () { 
+        openHours= ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: '];
         var cookieList = document.getElementById( 'pdxCookie' );
         console.log('container var is :' + cookieList)
             for ( var i = 0; i < 15; i++ ) {
             var newHourlyData = document.createElement( 'li' );
-            newHourlyData.innerHTML = this.locationCookieData()[i];
+            newHourlyData.innerHTML = (openHours[i] + (this.locationCookieData()[i]) + ' cookies') ;
             cookieList.appendChild( newHourlyData );
             }
         }
@@ -31,6 +32,6 @@ var pdxAirport = {
 
 pdxAirport.addToDom();
 
-openHours= ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12am: ']
+
 
 
