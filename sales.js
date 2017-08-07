c13b9311e9feaecbcb22e9d212a928caadad5f08
CHRISTINA MILLS <CHRISTINA.A.MILLS@GMAIL.COM>
2017-08-07 15:27:18 -0700

    added hourly cookie function, added location data, and started dom function

--- sales.js
+++ sales.js
@@ -8,7 +8,22 @@ var pdxAirport = {
     },
     totalHourlyCookies: function () {
         return this.hourlyCustomers() * this.avgSale
+    },
+    locationCookieData: function(){
+        var tempArr = []
+        for ( var i = 0; i < 15; i++ ) {
+       tempArr.push(Math.floor(this.totalHourlyCookies()));
+        }
+    return tempArr;
+    }
+
+    addToDom: function () { 
+        var cookieList = document.getElementById
+
     }
 };
+
+
+openHours= ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12am: ']
 
 
