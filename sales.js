d5fa4b27eb00f09077b339ce70da1bee5664364e
CHRISTINA MILLS <CHRISTINA.A.MILLS@GMAIL.COM>
2017-08-08 16:11:12 -0700

    added table row titles and populated with data

--- sales.js
+++ sales.js
@@ -1,98 +1,53 @@
 
-function BobStore ( storeLocation, minCustomer, maxCustomer, avgCookies, id, openHours) {
+function Store(storeLocation, minCustomer, maxCustomer, avgCookies, id, openHours) {
     this.storeLocation = storeLocation,
-    this.minCustomer = minCustomer,
-    this.maxCustomer = maxCustomer, 
-    this.avgCookies = avgCookies,
-    this.id = id,
-    this.cookiesPerHour = [],
-    this.openHours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: '];
+        this.minCustomer = minCustomer,
+        this.maxCustomer = maxCustomer,
+        this.avgCookies = avgCookies,
+        this.id = id,
+        this.cookiesPerHour = []
+        this.openHours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: '];
+        this.addToDom();
 }
 
-BobStore.prototype.calculateCust = function () {
+Store.prototype.calculateCust = function () {
     return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer);
 }
 
-BobStore.prototype.avgSalePerCustomer = function() {
+
+Store.prototype.avgSalePerCustomer = function () {
     return this.calculateCust() * this.avgCookies;
 }
 
+
 //total sales per hr
-BobStore.prototype.calcCookieData = function () {
+Store.prototype.calcCookieData = function () {
     for (var i = 0; i < this.openHours.length; i++) {
         this.cookiesPerHour.push(Math.floor(this.avgSalePerCustomer()));
-        }
+    }
     return this.cookiesPerHour;
-
 }
 
-BobStore.prototype.addToDom =  function () {
-        var listAdd = document.getElementById( 'id' );
-
+    Store.prototype.addToDom = function () {
         this.calcCookieData();
-        for (var i = 0; i < this.openHours.length; i++) {
-            var cookieLi = document.createElement('li');
-            cookieLi.innerHTML = (this.openHours[i] + this.cookiesPerHour[i] + ' cookies');
-            listAdd.appendChild(cookieLi);
+        var container = document.getElementById("stores");
+        var newRow = document.createElement('tr');
+        newRow.setAttribute('id', this.id);
+        newRow.innerHTML = this.storeLocation;
+        container.appendChild(newRow);
+        
+
+        for (var i = 0; i < 15; i++) {
+            var container = document.getElementById(this.id);
+            var newTableData = document.createElement('td')
+            newTableData.innerText = this.cookiesPerHour[i]; //[i]
+            container.appendChild(newTableData);
         }
     }
 
-var pdxAirport = new BobStore('PDX Airport', 23, 65, 6.3)
-var pioneerSquare = new BobStore('Pioneer Square', 3, 24, 1.2)
-var powells = new BobStore('Powell\'s', 11, 38, 3.7)
-var stJohns = new BobStore('St. John\'s', 20, 38, 3.7)
-var waterfront = new BobStore('Waterfront', 2, 16, 4.6)
 
-pdxAirport.storeLocation
-
-
-
-// var openHours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: '];
-
-// var pdxAirport = {
-//     location: 'PDX Airport',
-//     minCustomer: 23,
-//     maxCustomer: 65,
-//     avgCookies: 6.3,
-//     cookiesPerHour: [],
-//     totalCookies: 0,
-
-//     getTotal: function () {
-//         for (var i = 0; i < openHours.length; i++) {
-            
-//             this.totalCookies += this.cookiesPerHour[i];
-//         }
-//     },
-
-//     calculateCust: function () {
-//         return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer);
-//     },
-
-//     avgSalePerCustomer: function () {
-//         return this.calculateCust() * this.avgCookies;
-//     },
-
-//     calcCookieData: function () { 
-//         for (var i = 0; i < openHours.length; i++) {
-//             this.cookiesPerHour.push(Math.floor(this.avgSalePerCustomer()));
-//         }
-//     },
-
-//     addToDom: function () {
-//         var listAdd = document.getElementById( 'pdxCookie' );
-
-//         this.calcCookieData();
-//         for (var i = 0; i < openHours.length; i++) {
-//             var cookieLi = document.createElement('li');
-//             cookieLi.innerHTML = (openHours[i] + this.cookiesPerHour[i] + ' cookies');
-//             listAdd.appendChild(cookieLi);
-//         }
-
-//         this.getTotal();
-//         var totalLi = document.createElement('li');
-//         totalLi.innerText = ('Total Cookies: ' + this.totalCookies);
-//         listAdd.appendChild(totalLi);
-//     }
-// }
-
-// pdxAirport.addToDom();
+    var pdxAirport = new Store('PDX Airport', 23, 65, 6.3, 'pdx-airport');
+    var pioneerSquare = new Store('Pioneer Square', 3, 24, 1.2, 'pioneer-square');
+    var powells = new Store('Powell\'s', 11, 38, 3.7, 'powell');
+    var stJohns = new Store('St. John\'s', 20, 38, 3.7, 'st-johns');
+    var waterfront = new Store('Waterfront', 2, 16, 4.6, 'waterfront');
