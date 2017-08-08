3cbf68752c4c568da3ddb91fbe83005c0a9ff7f7
CHRISTINA MILLS <CHRISTINA.A.MILLS@GMAIL.COM>
2017-08-07 20:02:55 -0700

    revived original ststrategy but not working

--- sales.js
+++ sales.js
@@ -1,83 +1,56 @@
+
 var pdxAirport = {
     location: 'PDX Airport',
     minCustomer: 23, 
     maxCustomer: 65,
-    avgSale: 6.3,
-    hourlyCustomers: function () {
-        return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer ) + this.minCustomer)
-    },
-    cookiesPerHour: [],
+    avgSalePerCustomer: 6.3,
+    temporaryArr: [],
+    totalCookies: 0,
 
-    calculateCookies: function() {
-       for ( var i = 0; i < 15; i++ ) {
-       // console.log(this.hourlyCustomers())
-         
-        this.cookiesPerHour.push(Math.floor(this.hourlyCustomers() * this.avgSale))
-       }
-
-    },
-    total: 0,
-
-    cookiesPerDay: function () {
-        for (var i = 0; i < 15; i ++) {
-            this.total += cookiesPerHour;
-        }
-        console.log(total);
+    getTotal: function() {
+        for (var i = 0; i < 15; i++) {
+            this.totalCookies += this.temporaryArr[i];
+            }
     },
     
-    function 
-   
-    // firstAdd: function sum(a,b){ 
-    //     var total = a + b;
-    //     return total
-    // },
-
-    // findTotal: function sumArray(cookiesPerHour){
-    // var addedUp = sum( sum(cookiesPerHour[0], [1])[0], testArray[2])[0]
-    // return addedUp;
-    // },
-   
-   
+    calculate: function () {
+        return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer ) + this.minCustomer)
+    },
+    
+    avgSalePerCustomer: function() {
+        return this.calculate() * this.Avg;
+    },
+    
+    cookieData: function(){
+        for ( var i = 0; i < 15; i++ ) {
+        this.temporaryArr.push(Math.floor(this.avgSalePerCustomer() ) );
+        }
+        console.log(temporaryArr);
+        
+    },
 
     addToDom: function () { 
         openHours= ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: '];
-        var cookieList = document.getElementById( 'pdxCookie' );
-        console.log('container var is :' + cookieList)
-        this.calculateCookies();
+        var listAdd = document.getElementById( 'pdxCookie' );
+     
             for ( var i = 0; i < 15; i++ ) {
                 if (i < 15) {
-                    
-                    cookieList.innerHTML = (openHours[i] + this.cookiesPerHour[i] + ' cookies');
-                    cookieList.appendChild( cookieList );
+                    var newList = document.createElement('li');
+                    newList.innerHTML = (openHours[i] + this.cookieData()[i] + ' cookies');
+                    listAdd.appendChild( newList );
                 }
                 else {
-            var newHourlyData = document.createElement( 'li' );
-            this.cookiesPerDay();
-            cookieList.innerText = ('Total Cookies: ' + this.cookiesPerDay);
-            cookieList.appendChild( cookieList );
-            }
+                    var newList = document.createElement('li');
+                    this.getTotal();
+                    newList.innerText = ('Total Cookies: ' + this.totalCookies);
+                    listAdd.appendChild(newList);
+                }    
             
-    
-    
-            }
-    }
-
+        
+        }
+    },
 }
 
 
 pdxAirport.addToDom();
-
-
- // totalHourlyCookies: function () {
-    //     return this.hourlyCustomers() * this.avgSale
-    // },
-
-    // locationCookieData: function(){
-    //     var tempArr = []
-    //     for ( var i = 0; i < 15; i++ ) {
-    //     tempArr.push(Math.floor(this.totalHourlyCookies()));
-    //     }
-    //     return tempArr;
-    // },
-    
 
